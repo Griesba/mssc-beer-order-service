693dde4d63d32cb8087a9964f339dfc923c10984
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-23 19:15:21 +0200

    updating BeerOrderLineDto with Beer information from beer service

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -0,0 +1,59 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Service
+public class BeerServiceImpl implements BeerService {
+    private static final String BEER_UPC_SERVICE_PATH = "/api/v1/beerUpc/";
+    private final static String BEER_ID_SERVICE_PATH = "/api/v1/beer/";
+    private String beerServiceHost;
+
+    private final RestTemplate restTemplate;
+
+    public BeerServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public void setBeerServiceHost(String host){
+        this.beerServiceHost = host;
+    }
+
+    public Optional<BeerDto> getBeerById(UUID uuid){
+        String uri = beerServiceHost + BEER_ID_SERVICE_PATH + uuid.toString();
+        return Optional.of(restTemplate.getForObject(uri, BeerDto.class));
+    }
+
+    public Optional<BeerDto> getBeerByUpc(String upc){
+        String uri = beerServiceHost + BEER_UPC_SERVICE_PATH + upc;
+        log.debug("Calling Beer Service: "+ uri);
+        return Optional.of(restTemplate.getForObject(uri,BeerDto.class));
+    }
+
+    public BeerDto getBeerDto(UUID beerId){
+
+        log.debug("Calling Beer Service");
+
+        String uri = beerServiceHost + BEER_ID_SERVICE_PATH + beerId;
+        ResponseEntity<BeerDto> responseEntity = restTemplate.exchange(uri,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<BeerDto>(){},
+                (Object) beerId);
+
+        return Objects.requireNonNull(responseEntity.getBody());
+    }
+}
---
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOderLineMapperDecorator.java
@@ -0,0 +1,43 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.services.beer.BeerService;
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import guru.sfg.beer.order.service.web.model.BeerOrderLineDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.Optional;
+
+public abstract class BeerOderLineMapperDecorator implements BeerOrderLineMapper{
+
+    private BeerService beerService;
+    private BeerOrderLineMapper mapper;
+
+    @Autowired
+    public void setBeerService(BeerService beerService){
+        this.beerService = beerService;
+    }
+
+    @Autowired
+    @Qualifier("delegate")
+    public void setBeerOrderLineMapper(BeerOrderLineMapper beerOrderLineMapper){
+        this.mapper = beerOrderLineMapper;
+    }
+
+    @Override
+    public BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line) {
+        BeerOrderLineDto orderLineDto = mapper.beerOrderLineToDto(line);
+        Optional<BeerDto> optionalBeerDto = beerService.getBeerByUpc(line.getUpc());
+
+        optionalBeerDto.ifPresent(beerDto -> {
+            orderLineDto.setBeerName(beerDto.getBeerName());
+            orderLineDto.setBeerStyle(beerDto.getBeerStyle());
+            orderLineDto.setPrice(beerDto.getPrice());
+            orderLineDto.setBeerId(beerDto.getId());
+        });
+        return orderLineDto;
+    }
+
+}
